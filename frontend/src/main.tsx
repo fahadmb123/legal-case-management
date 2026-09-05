b02eab97d6ad62a3ba5284174b0548b6755a7124
fahadmb123 <fahadmbasheer@gmail.com>
2026-09-06 01:58:29 +0530

    feat : add styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,6 @@
+import "./styles/variables.css";
+import "./styles/themes.css";
+import "./styles/global.css";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
