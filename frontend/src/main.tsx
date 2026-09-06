0af9d8f78ac629eabc19ad62ac9fc0e3d524696e
fahadmb123 <fahadmbasheer@gmail.com>
2026-09-06 12:03:22 +0530

    feat : add them context

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,8 +6,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import { ThemeProvider } from "./context/ThemeContext.tsx";
+
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
