3102332533287f0324846c2db4ef0abc30f89efd
fahadmb123 <fahadmbasheer@gmail.com>
2026-09-11 00:15:59 +0530

    feat : Login use case

---
+++ backend/src/application/use-cases/auth/LoginUseCase.ts
@@ -0,0 +1,42 @@
+import type { IUserRepository } from "../../../domain/repositories/IUserRepository";
+
+export interface IPasswordVerifier {
+  verify(password: string, passwordHash: string): Promise<boolean>
+}
+
+export interface ITokenService {
+  generate(userId: string): string
+}
+
+export class LoginUseCase {
+  constructor(
+    private readonly userRepository: IUserRepository,
+    private readonly passwordVerifier: IPasswordVerifier,
+    private readonly tokenService: ITokenService
+  ) {}
+
+    async execute(email: string, password: string) {
+        const user = await this.userRepository.findByEmail(email)
+
+        if (!user) throw new Error("Invalid email or password")
+        
+
+        const isPasswordValid = await this.passwordVerifier.verify(
+            password,
+            user.passwordHash
+        )
+
+        if (!isPasswordValid) throw new Error("Invalid email or password")
+
+        const token = this.tokenService.generate(user.id)
+
+        return {
+        user: {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+        },
+        token,
+        }
+    }
+}
--- backend/src/application/use-cases/auth/RegisterUseCase.ts
+++ backend/src/application/use-cases/auth/RegisterUseCase.ts
@@ -11,22 +11,22 @@ export class RegisterUseCase {
     private readonly passwordHasher: IPasswordHasher
   ) {}
 
-  async execute(name: string,email: string,password: string): Promise<User> {
+    async execute(name: string,email: string,password: string): Promise<User> {
 
-    const existingUser = await this.userRepository.findByEmail(email)
+        const existingUser = await this.userRepository.findByEmail(email)
 
-    if (existingUser) throw new Error("User with this email already exists")
+        if (existingUser) throw new Error("User with this email already exists")
 
-    const passwordHash = await this.passwordHasher.hash(password)
+        const passwordHash = await this.passwordHasher.hash(password)
 
-    const user: User = {
-      id: crypto.randomUUID(),
-      name,
-      email,
-      passwordHash,
-      createdAt: new Date(),
-    };
+        const user: User = {
+        id: crypto.randomUUID(),
+        name,
+        email,
+        passwordHash,
+        createdAt: new Date(),
+        };
 
-    return this.userRepository.create(user)
-  }
+        return this.userRepository.create(user)
+    }
 }
